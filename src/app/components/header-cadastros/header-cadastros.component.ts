20ee95b854f8997632bef97bb38d44e7db84977d
_julianogueira3 <julia.mjn@icloud.com>
2023-09-26 18:24:29 -0300

    header e login

--- src/app/components/header-cadastros/header-cadastros.component.ts
+++ src/app/components/header-cadastros/header-cadastros.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-header-cadastros',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./header-cadastros.component.sass']
 })
 export class HeaderCadastrosComponent {
+  constructor(private router: Router) {}
 
+  redirectToHome() {
+    // Adicione o código para redirecionar para a página "Home"
+    this.router.navigate(['/']);
+  }
+
+  redirectToLogin() {
+    // Adicione o código para redirecionar para a página de login
+    this.router.navigate(['/login-page']);
+  }
 }
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.sass']
+})
+export class LoginComponent {
+  loginForm: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      senha: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      // Lógica de autenticação aqui
+    }
+  }
+}
